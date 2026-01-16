a4e3a357013bfae35404dfd01fd241b5b2dc6674
jhm-cmyk <jhmmk05@gmail.com>
2026-01-16 19:43:49 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'noor-hussein-v3';
+const ASSETS = ['/', '/index.html', '/style.css', '/script.js', '/manifest.json'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
+
+// حذف الكاش القديم لتفعيل التحديث الجديد فوراً
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
